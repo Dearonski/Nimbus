728420ffdb92de5413fcbee60ae00750b5477e04
Dearonski <abromovich546@gmail.com>
2026-07-09 02:16:01 +0300

    Initial Commit

---
+++ Nimbus/NimbusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimbusApp.swift
+//  Nimbus
+//
+//  Created by Кирилл on 09.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct NimbusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
